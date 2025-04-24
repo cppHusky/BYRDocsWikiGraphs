a7bad5786b245e419d369224142d728b5bf9268d
cppHusky <cpphusky@outlook.com>
2025-04-24 21:41:53 +0800

    added upload command modified:   uploader/CMakeLists.txt new file:   uploader/src/upload.cpp

---
+++ uploader/src/upload.cpp
@@ -0,0 +1,62 @@
+#include<cstdlib>
+#include<exception>
+#include<filesystem>
+#include<format>
+#include<fstream>
+#include<iostream>
+#include<string>
+#include<nlohmann/json.hpp>
+#include"wiki.hpp"
+const std::string API="http://localhost:8080/api.php";
+const std::string LOGIN="/tmp/wiki_login_cookies";
+const std::string CSRF="/tmp/wiki_csrf_cookies";
+const std::list<std::string> HEADER{std::format("X-Byrdocs-Token:{}",std::getenv("WIKITOKEN"))};
+int main(int argc,char *argv[]){
+	try{
+		std::string file_path{argv[1]};
+		std::filesystem::path fs_path{file_path};
+		if(argc!=2){
+			std::cout<<std::format(R"(Usage: {} <path-to-file>)",argv[0])<<std::endl;
+			return 1;
+		}
+		const std::string login_token=wiki::get_login_token(API,LOGIN);
+		wiki::login(
+			API,
+			LOGIN,
+			"CppHusky@uploader",
+			std::getenv("WIKIPASS_UPLOADER"),
+			login_token
+		);
+		std::clog<<std::format("Logged in. Started uploading {}...",argv[1])<<std::endl;
+		const std::string csrf_token=wiki::get_csrf_token(API,LOGIN,CSRF);
+		if(!std::filesystem::exists(fs_path)){
+			std::cerr<<std::format("{}: file does not exist.",file_path)<<std::endl;
+			return 2;
+		}
+		if(std::filesystem::is_directory(fs_path)){
+			std::cerr<<std::format("{}: is a directory.",file_path)<<std::endl;
+			return 3;
+		}
+		std::string filename{std::filesystem::path{file_path}.filename()};
+		nlohmann::json result=wiki::upload(
+			API,
+			CSRF,
+			filename,
+			file_path,
+			csrf_token,
+			HEADER
+		);
+		std::clog<<result<<std::endl;
+		if(result["upload"]["result"]!="Success")
+			return 3;
+	}catch(const curlpp::RuntimeError &e){
+		std::cerr<<e.what()<<std::endl;
+		assert(false);
+	}catch(const curlpp::LogicError &e){
+		std::cerr<<e.what()<<std::endl;
+		assert(false);
+	}catch(const std::exception &e){
+		std::cerr<<e.what()<<std::endl;
+		assert(false);
+	}
+}
