cc5b757c64348fa61f3dde92a714f54f442ca5bb
cppHusky <cpphusky@outlook.com>
2025-04-25 16:31:01 +0800

    test uploading workflow new file:   .github/workflows/upload-new-graph.yml modified:   uploader/src/upload.cpp

--- uploader/src/upload.cpp
+++ uploader/src/upload.cpp
@@ -7,25 +7,26 @@
 #include<string>
 #include<nlohmann/json.hpp>
 #include"wiki.hpp"
-const std::string API="http://localhost:8080/api.php";
+const std::string API="https://wiki.byrdocs.org/api.php";
 const std::string LOGIN="/tmp/wiki_login_cookies";
 const std::string CSRF="/tmp/wiki_csrf_cookies";
 const std::list<std::string> HEADER{std::format("X-Byrdocs-Token:{}",std::getenv("WIKITOKEN"))};
 int main(int argc,char *argv[]){
 	try{
-		std::string file_path{argv[1]};
-		std::filesystem::path fs_path{file_path};
 		if(argc!=2){
 			std::cout<<std::format(R"(Usage: {} <path-to-file>)",argv[0])<<std::endl;
 			return 1;
 		}
+		std::string file_path{argv[1]};
+		std::filesystem::path fs_path{file_path};
 		const std::string login_token=wiki::get_login_token(API,LOGIN);
 		wiki::login(
 			API,
 			LOGIN,
 			"CppHusky@uploader",
 			std::getenv("WIKIPASS_UPLOADER"),
-			login_token
+			login_token,
+			HEADER
 		);
 		std::clog<<std::format("Logged in. Started uploading {}...",argv[1])<<std::endl;
 		const std::string csrf_token=wiki::get_csrf_token(API,LOGIN,CSRF);
@@ -46,9 +47,11 @@ int main(int argc,char *argv[]){
 			csrf_token,
 			HEADER
 		);
-		std::clog<<result<<std::endl;
-		if(result["upload"]["result"]!="Success")
-			return 3;
+		if(result["upload"]["result"]!="Success"){
+			std::clog<<"The uploader bot encountered a non-success result. See the log in current directory for more information."<<std::endl;
+			std::ofstream logs{std::format("{}.log",filename)};
+			logs<<result<<std::endl;
+		}
 	}catch(const curlpp::RuntimeError &e){
 		std::cerr<<e.what()<<std::endl;
 		assert(false);
